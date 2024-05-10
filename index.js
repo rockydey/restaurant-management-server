5222a6d78aed801dfffa8e60e8b17a59b3dc8507
Rocky Dey <coderrocky25@gmail.com>
2024-05-10 23:23:16 +0600

    get food details by id using get api implemented

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -26,6 +26,13 @@ async function run() {
 
     app.get("/foods", async (req, res) => {
       const result = await foodsCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await foodsCollection.findOne(query);
       res.send(result);
     });
 
